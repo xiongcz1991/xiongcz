f41f19d3cbcfad38cb3c1cc2cfc488ee65c617f5
xiongcz <xiongcz1991@163.com>
2019-04-13 11:46:00 +0800

    ZOO TEST

--- java-demo/parent/zoo/src/main/java/com/xiongcz/demo/parent/App.java
+++ java-demo/parent/zoo/src/main/java/com/xiongcz/demo/parent/App.java
@@ -1,13 +1,25 @@
 package com.xiongcz.demo.parent;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+
+    public static void main( String[] args ){
+
+        try {
+            ZooKeeper zooKeeper = new ZooKeeper("127.0.0.1",120,null);
+            zooKeeper.create("/name","xiongcz".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
